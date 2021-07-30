a0bd404c55aa6bb6ffee4364a07ce154a483969e
natcecilio <tatafly2009@hotmail.com>
2021-07-30 10:25:16 -0300

    att text about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -13,13 +13,13 @@ function About () {
     
       <p>
         <img className="about-picture" src={"images/about.jpg" } alt="Luca" width="190vw" height="auto"/>
-        My name is Luca, I have over 20 years of experience in the 3D industry that have allowed me to successfully work for local and international Creative Agencies
+        My name is Luca, I have over 35 years of experience in the 3D industry that have allowed me to successfully work for local and international Creative Agencies
 
         Some of my specialties as a Professional 3D Artist include 3D product visualizations and renderings, 3D animations and motion projects for print, web and video.
 
         I would love to discuss how we can work together! contact me
 
-        I'm curious about your project!.
+        I'm curious about your project!
       </p>
     </div>
   );
